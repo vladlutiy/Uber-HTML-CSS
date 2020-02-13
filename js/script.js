810a1d0e694f7a7ca2627be726584e2ddeb7d664
Vlad <chillout.ua.mail@gmail.com>
2020-02-13 12:11:21 +0100

    finish

--- js/script.js
+++ js/script.js
@@ -24,18 +24,61 @@ window.addEventListener('DOMContentLoaded', () => {
               $('.contacts').eq(i).toggleClass('contacts_plus');
             });
         });
-    };
+    }
     
     toggleSlide('.contacts_link_bottom');
     toggleSlide('.contacts_link');
     toggleSlide('.contacts_close');
-    
 
-    // $(hamburger).each(function() {
-    //     $(this).on('click', function(e){
-    //       e.preventDefault();
-    //       $('.contacts').toggleClass('contacts_ham');
-    //     });
-    // });
+    $('[data-modal="consultation"]').on('click',function(){
+        $('.overlay, #consultation').fadeIn();
+    });
+
+    $('.modal_close').on('click', function(){
+        $('.overlay, #consultation').fadeOut();
+    });
+
+    $('#consultation form').validate({
+        rules: {
+            name: "required",
+            phone: {
+                required: true,
+                minlength: 9
+            },
+            email: {
+                required: true,
+                email: true
+            }
+        },
+        messages: {
+            name: "Пожалуйста, впишите свое имя",
+            phone :{
+                required: "Пожалуйста, впишите номер телефона",
+                minlength: "Номер должен состояти минимум из {0} цифр"
+            },
+            email :{
+                required: "Пожалуйста, впишите свой e-mail",
+                email: "Ваш  e-mail адресс должен быть формата - name@domain.com"
+            }
+        }
+    });
+
+    $('input[name=phone]').mask("+ 48 (999) 999 999");
+
+    $('form').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $('#consultation').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+    
+            $('form').trigger('reset');
+        });
+        return false;
+      });
 
 });
